c6e831cc656b83015d7879e51009c358e848105d
Sujit <sujitkar1195@gmail.com>
2024-04-17 14:53:51 +0530

    version 1.0.0

---
+++ src/components/Square.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Square({value, onSquareClick }){
+	
+	return (
+		<>
+			<button className="square" onClick={onSquareClick} >
+				{value}
+			</button>
+		</>
+	);
+}
