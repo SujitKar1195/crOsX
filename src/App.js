623a9313e984a728642e88a080fed839d678034f
Sujit <sujitkar1195@gmail.com>
2024-04-17 15:06:30 +0530

    version 1.0.1

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import './App.css';
 import Board from './components/Board';
 function App() {
   return (
-    <div className="App">
-      <Board/>
+    <div className='App'>
+      <h1 className='text-center text-light pt-2'>crOsX</h1>
+      <Board />
     </div>
   );
 }
